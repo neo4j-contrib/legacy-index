ef3c61eb31b701ef4b55bd053830e1c20904b3c3
Johan Svensson <johan@neotechnology.com>
2008-04-07 09:44:41 +0000

    Documented IndexService.
    git-svn-id: https://svn.neo4j.org/components/index-util/trunk@1034 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- src/main/java/org/neo4j/util/index/IndexService.java
+++ src/main/java/org/neo4j/util/index/IndexService.java
@@ -2,17 +2,67 @@ package org.neo4j.util.index;
 
 import org.neo4j.api.core.Node;
 
+/**
+ * An index service can be used to index nodes with a key and a value. 
+ */
 public interface IndexService
 {
+    /**
+     * Index <code>node</code> with <code>key</code> and <code>value</code>.
+     * 
+     * @param node node to index
+     * @param key key for the index
+     * @param value value to index the node with
+     */
     void index( Node node, String key, Object value );
 
+    /**
+     * Returns a single node indexed with <code>key</code> and 
+     * <code>value</code>. If no such index exist <code>null</code> is 
+     * returned. If more then one node is found a runtime exception is 
+     * thrown.
+     * 
+     * @param key key for index
+     * @param value value for index
+     * @return node that has been indexed with key and value or 
+     * <code>null</code>
+     */
     Node getSingleNode( String key, Object value );
 
+    /**
+     * Returns all nodes indexed with <code>key</code> and 
+     * <code>value</code>.
+     * 
+     * @param key key for index
+     * @param value value for index
+     * @return nodes that has been indexed with key and value
+     */
     Iterable<Node> getNodes( String key, Object value );
 
+    /**
+     * Removes a index for a node. If no such indexing exist this method 
+     * silently returns.
+     * 
+     * @param node node to remove indexing from
+     * @param key key of index
+     * @param value value of index
+     */
     void removeIndex( Node node, String key, Object value );
     
+    /**
+     * Changes isolation level for the running transaction. This method must
+     * be invoked before any index (add/remove) has been performed in the 
+     * current transaction. Default isolation level is 
+     * {@link Isolation#SAME_TX}.
+     * 
+     * @param level new isolation level
+     */
     void setIsolation( Isolation level );
 
+    /**
+     * Stops this indexing service comitting any asynchronous requests that 
+     * are currently queued. After this method has been invoked any following
+     * method invoke on this instance is invalid.
+     */
     void shutdown();
 }
