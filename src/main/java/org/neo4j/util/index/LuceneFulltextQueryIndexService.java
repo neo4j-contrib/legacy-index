40e5a4623df4f9a84b1e4860d1267080dbf06451
Mattias Persson <mattias@neotechnology.com>
2009-08-25 15:15:21 +0000

    LuceneFulltextIndexService which interprets the value in getNodes method as a lucene query syntax for more flexible expressions
    git-svn-id: https://svn.neo4j.org/components/index-util/trunk@3077 0b971d98-bb2f-0410-8247-b05b2b5feb2a

---
+++ src/main/java/org/neo4j/util/index/LuceneFulltextQueryIndexService.java
@@ -0,0 +1,33 @@
+package org.neo4j.util.index;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.WhitespaceAnalyzer;
+import org.apache.lucene.queryParser.ParseException;
+import org.apache.lucene.queryParser.QueryParser;
+import org.apache.lucene.search.Query;
+import org.neo4j.api.core.NeoService;
+
+public class LuceneFulltextQueryIndexService extends LuceneFulltextIndexService
+{
+    private static final Analyzer WHITESPACE_ANALYZER =
+        new WhitespaceAnalyzer();
+    
+    public LuceneFulltextQueryIndexService( NeoService neo )
+    {
+        super( neo );
+    }
+
+    @Override
+    protected Query formQuery( String key, Object value )
+    {
+        try
+        {
+           return new QueryParser( DOC_INDEX_KEY,
+              WHITESPACE_ANALYZER ).parse( value.toString() );
+        }
+        catch ( ParseException e )
+        {
+           throw new RuntimeException( e );
+        }
+    }
+}
