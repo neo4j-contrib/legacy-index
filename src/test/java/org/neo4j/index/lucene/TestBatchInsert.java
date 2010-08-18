fa7c0ceba9e30186c830d54c8b1ceef711e325ea
Tobias Ivarsson <tobias.ivarsson@neotechnology.com>
2010-08-18 10:26:19 +0000

    Moved the shell index command to the (two) index component(s).
    git-svn-id: https://svn.neo4j.org/components/index/trunk@5242 0b971d98-bb2f-0410-8247-b05b2b5feb2a

--- src/test/java/org/neo4j/index/lucene/TestBatchInsert.java
+++ src/test/java/org/neo4j/index/lucene/TestBatchInsert.java
@@ -3,17 +3,17 @@
  *     Network Engine for Objects in Lund AB [http://neotechnology.com]
  *
  * This file is part of Neo4j.
- * 
+ *
  * Neo4j is free software: you can redistribute it and/or modify
  * it under the terms of the GNU Affero General Public License as
  * published by the Free Software Foundation, either version 3 of the
  * License, or (at your option) any later version.
- * 
+ *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU Affero General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU Affero General Public License
  * along with this program. If not, see <http://www.gnu.org/licenses/>.
  */
@@ -41,14 +41,14 @@ public class TestBatchInsert
     {
         return "target/var/batch-insert";
     }
-    
+
     @Before
     public void setUpBatchInserter() throws Exception
     {
         Neo4jTestCase.deleteFileOrDirectory( new File( getDbPath() ) );
         inserter = new BatchInserterImpl( getDbPath() );
     }
-    
+
     @After
     public void tearDownBatchInserter()
     {
@@ -59,17 +59,17 @@ public class TestBatchInsert
         }
         inserter.shutdown();
     }
-    
+
     @Test
     public void testSimpleBatchInsert()
     {
         index = new LuceneIndexBatchInserterImpl( inserter );
         assertEquals( inserter, index.getBatchInserter() );
         long node = inserter.createNode( null );
-        assertTrue( !index.getNodes( "test-key", 
+        assertTrue( !index.getNodes( "test-key",
             "test-value" ).iterator().hasNext() );
         index.index( node, "test-key", "test-value" );
-        assertTrue( index.getNodes( "test-key", 
+        assertTrue( index.getNodes( "test-key",
             "test-value" ).iterator().hasNext() );
     }
 
@@ -78,12 +78,12 @@ public class TestBatchInsert
     {
         index = new LuceneFulltextIndexBatchInserter( inserter );
         long node = inserter.createNode( null );
-        assertTrue( !index.getNodes( "test-key", 
+        assertTrue( !index.getNodes( "test-key",
             "test-value" ).iterator().hasNext() );
         index.index( node, "test-key", "test-value" );
-        assertTrue( index.getNodes( "test-key", 
+        assertTrue( index.getNodes( "test-key",
             "test-value" ).iterator().hasNext() );
-        
+
         String value = "A decent value for indexing";
         String key = "my key";
         index.index( node, key, value );
@@ -94,7 +94,7 @@ public class TestBatchInsert
         assertFalse( index.getNodes( key,
             "abcdefghijklmnop" ).iterator().hasNext() );
     }
-    
+
     @Test
     public void testMoreFulltextBatchInsert()
     {
@@ -107,7 +107,7 @@ public class TestBatchInsert
             assertEquals( i, index.getSingleNode( "mykey1", i ) );
         }
         long slowTime = System.currentTimeMillis() - time;
-        
+
         // Should be much faster
         time = System.currentTimeMillis();
         for ( int i = 0; i < 1000; i++ )
@@ -120,9 +120,11 @@ public class TestBatchInsert
             assertEquals( i, index.getSingleNode( "mykey2", i ) );
         }
         long fastTime = System.currentTimeMillis() - time;
-        assertTrue( fastTime < slowTime / 5 );
+        assertTrue( String.format(
+                "optimized mode should be faster [%s < %s / 5]", fastTime,
+                slowTime ), fastTime < slowTime / 5 );
     }
-    
+
     @Test
     public void testHmm() throws Exception
     {
@@ -145,7 +147,7 @@ public class TestBatchInsert
             index.index( imageIdStart + i, "TYPE", "type" );
         }
         index.optimize();
-        
+
         for ( int i = 0; i < 100; i++ )
         {
             long tail = index.getSingleNode( "ID_TITLE", i );
@@ -160,7 +162,7 @@ public class TestBatchInsert
     {
         index = new LuceneIndexBatchInserterImpl( inserter );
         long node = inserter.createNode( null );
-        assertTrue( index.getSingleNode( "test-key", "test-value" ) == -1 ); 
+        assertTrue( index.getSingleNode( "test-key", "test-value" ) == -1 );
         index.index( node, "test-key", "test-value" );
         index.shutdown();
         inserter.shutdown();
